8e6990a120df0663bbc4b27fcccfe840dedb2dd4
joseraulsoriano <joseraulsoriano@github.com>
2025-11-28 19:10:01 -0600

    Separar backend a repositorio independiente

--- back/analytics/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import statistics, category_stats
-
-urlpatterns = [
-    path('', statistics, name='statistics'),
-    path('category/<int:category_id>/', category_stats, name='category-stats'),
-]
-
-
